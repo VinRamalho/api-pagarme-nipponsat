d4af3816333bdcd5df9cc19914c3e83e5d3f3932
Vinicius Ramalho <engenharia01@nipponsat.com.br>
2022-06-27 09:47:06 -0300

    organizando pastas

---
+++ api/php/test.php
@@ -0,0 +1,33 @@
+<?php
+
+$curl = curl_init();
+
+curl_setopt_array($curl, [
+  CURLOPT_URL => "https://api.pagar.me/core/v5/orders",
+  CURLOPT_RETURNTRANSFER => true,
+  CURLOPT_ENCODING => "",
+  CURLOPT_MAXREDIRS => 10,
+  CURLOPT_TIMEOUT => 30,
+  CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
+  CURLOPT_CUSTOMREQUEST => "POST",
+  CURLOPT_POSTFIELDS => "
+  {
+    \"items\":[{\"amount\":2200,\"description\":\"rastreador\",\"quantity\":1,\"code\":\"1\"}],\"customer\":{\"code\":\"1\",\"document\":\"11111111111\",\"address\":{\"country\":\"BR\",\"state\":\"PR\",\"city\":\"Curitiba\",\"zip_code\":\"81570170\",\"line_1\":\"4, Machado de assis, Uberaba\",\"line_2\":\"casa\"},\"name\":\"Francisco\",\"type\":\"individual\",\"email\":\"test@test.com\",\"document_type\":\"CPF\"},\"payments\":[{\"payment_method\":\"checkout\",\"checkout\":{\"accepted_payment_methods\":[\"credit_card\",\"boleto\",\"bank_transfer\",\"debit_card\",\"pix\"],\"customer_editable\":\"false\",\"boleto\":{\"due_at\":\"2022/06/28T09:07:50Z\"},\"success_url\":\"https://www.nps-solutions.com.br\",\"debit_card\":{\"authentication\":{\"statement_descriptor\":\"Desc na fatura\",\"type\":\"threed_secure\",\"threed_secure\":{\"mpi\":\"acquirer\",\"success_url\":\"https://www.nps-solutions.com.br\"}}},\"bank_transfer\":{\"bank\":[\"033\",\"001\",\"104\",\"237\",\"341\"]},\"credit_card\":{\"capture\":\"true\",\"operation_type\":\"auth_and_capture\"}}}]
+  }",
+  CURLOPT_HTTPHEADER => [
+    "Accept: application/json",
+    "Authorization: Basic c2tfdGVzdF92ZTkzZHdjOExJd2JqMVlWOg==",
+    "Content-Type: application/json"
+  ],
+]);
+
+$response = curl_exec($curl);
+$err = curl_error($curl);
+
+curl_close($curl);
+
+if ($err) {
+  echo "cURL Error #:" . $err;
+} else {
+  echo $response;
+}
